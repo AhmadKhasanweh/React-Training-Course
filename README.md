ca55687a95d106d851d95407a416e898ec66c204
AhmadKhasanweh <ahmad.khasawneh@hotmail.com>
2017-05-27 18:57:18 +0300

    Add gitignore

--- README.md
+++ README.md
@@ -1,3 +1,2 @@
 # React-Training-Course
 from reacttraining.com
-sa
---
+++ webpack.config.js
@@ -0,0 +1,16 @@
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+module.exports = {
+  'entry': './app/index.js',
+  module: {
+    rules: []
+  },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'index_bundle.js'
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './app/index.html'
+    })
+  ]
+}
