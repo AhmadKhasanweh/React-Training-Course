80a2fc0cd0c0ba7ef0d307571fdb9c9f82df53ba
AhmadKhasanweh <ahmad.khasawneh@hotmail.com>
2017-05-30 16:04:57 +0300

    change to stateless component

--- app/components/Popular.js
+++ app/components/Popular.js
@@ -1,11 +1,38 @@
 var React = require('react');
+var PropTypes = require('prop-types');
+
+function SelectLanguage(props) {
+  var languages = ['All', 'JavaScript', 'Java', 'Ruby', 'Python', 'CSS'];
+
+  return (
+    <ul className='languages'>
+      {
+        languages.map((lang) => {
+          return (
+            <li
+            style={lang === props.selectedLanguage ? {color: '#d0021b'} : null}
+            onClick={props.updateLanguage.bind(null, lang)}
+            key={lang}>
+              {lang}
+            </li>
+          )
+        })
+      }
+    </ul>
+  )
+}
+
+SelectLanguage.propTypes = {
+  selectedLanguage: PropTypes.string.isRequired,
+  updateLanguage: PropTypes.func.isRequired,
+};
 
 class Popular extends React.Component {
 
   constructor(props) {
     super(props);
     this.state = {
-      selectedLangauge: 'All'
+      selectedLanguage: 'All'
     }
 
     this.updateLanguage = this.updateLanguage.bind(this);
@@ -14,28 +41,19 @@ class Popular extends React.Component {
   updateLanguage(lang) {
     this.setState(()=>{
       return {
-        selectedLangauge: lang
+        selectedLanguage: lang
       }
     });
   }
 
   render() {
-    var languages = ['All', 'JavaScript', 'Java', 'Ruby', 'Python', 'CSS'];
     return (
-      <ul className='languages'>
-        {
-          languages.map((lang) => {
-            return (
-              <li
-              style={lang === this.state.selectedLangauge ? {color: '#d0021b'} : null}
-              onClick={this.updateLanguage.bind(null, lang)}
-              key={lang}>
-                {lang}
-              </li>
-            )
-          })
-        }
-      </ul>
+      <div>
+        <SelectLanguage
+        selectedLanguage={this.state.selectedLanguage}
+        updateLanguage={this.updateLanguage}
+        />
+      </div>
     )
   }
 }
